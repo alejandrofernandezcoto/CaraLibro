ca264fdbd959d4ac8217c4fb03e659d93da1941a
alejandrofernandezcoto <119857332+alejandrofernandezcoto@users.noreply.github.com>
2022-12-13 12:37:28 +0100

    Update XeradorMenus.java

--- src/main/java/com/mycompany/caralibrobd/XeradorMenus.java
+++ src/main/java/com/mycompany/caralibrobd/XeradorMenus.java
@@ -5,7 +5,7 @@
 package com.mycompany.caralibrobd;
 import java.util.Scanner;
 import java.util.Collection;
-
+import java.util.ArrayList;
 /**
  *
  * @author a22alejandrofc
@@ -15,13 +15,16 @@ public class XeradorMenus {
     
     public void crearPerfil(){
       
-        String nome;
+       String nome;
         String contrasinal;
-        
         Scanner entrada = new Scanner(System.in);
-        
-        nome=entrada.nextLine();
-        contrasinal=entrada.nextLine();
+
+        System.out.println("Introduzca un nombre de usuario\n");
+        nome = entrada.nextLine();
+        System.out.println("Introduzca una contraseña válida\n");
+        contrasinal = entrada.nextLine();
+        Perfil novoPerfil = new Perfil(nome, contrasinal);
+        Collection.add(novoPerfil);
        
        
                 
